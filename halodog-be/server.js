95be4e257e52736c718603bf34c02d5a4419f2f2
daffaromero <daffaromero@mail.ugm.ac.id>
2023-03-21 13:21:52 +0700

    Starting backend + MongoDB

---
+++ halodog-be/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
+//Enable CORS requests
+// app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
