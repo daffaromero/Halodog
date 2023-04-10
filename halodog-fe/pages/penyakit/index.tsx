eb82f9adfc91d9d6eb8501a0521e1c03ef3f8261
daffaromero <daffaromero@mail.ugm.ac.id>
2023-04-10 10:11:09 +0700

    FE: Added React component to /penyakit to prevent build error

---
+++ halodog-fe/pages/penyakit/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+function Penyakit() {
+  const [disease, setDisease] = useState({});
+  
+  return (
+    <div>
+      <h1>
+        Daftar Penyakit Goes Here
+      </h1>
+    </div>
+  )
+}
+
+export default Penyakit
