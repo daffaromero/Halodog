8d0807e94122def6b38664f36266c4e373f9162d
daffaromero <daffaromero@mail.ugm.ac.id>
2023-04-22 18:36:52 +0700

    Seeder, filtering, sorting, and pagination

--- halodog-be/controllers/diseases.js
+++ halodog-be/controllers/diseases.js
@@ -6,11 +6,65 @@ const ErrorResponse = require("../utils/errorResponse");
 // @route   GET /api/v1/diseases
 // @access  Public
 exports.getAllDiseases = asyncHandler(async (req, res, next) => {
-  const diseases = await Disease.find();
+  let query;
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ["select", "sort", "page", "limit"];
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery); // Create query string
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `${match}`); // Create operators
+  query = Disease.find(JSON.parse(queryStr)); // Finding resource obv
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  // Sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Disease.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  const diseases = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
 
   res.status(200).json({
     success: true,
     count: diseases.length,
+    pagination,
     data: diseases,
   });
 });
---
+++ halodog-be/seeder.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env variables
+dotenv.config({ path: "./config/.env" });
+
+// Load models
+const Disease = require("./models/Disease");
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Read JSON files
+const diseases = JSON.parse(
+  fs.readFileSync(`${__dirname}/_placeholder/diseases.json`, "utf-8")
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Disease.create(diseases);
+
+    console.log("Data successfully imported!".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data in DB
+const deleteData = async () => {
+  try {
+    await Disease.deleteMany();
+
+    console.log("Data destroyed!".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
