19fa5855871905fe2af76f81b68e9c154c380379
MuhamadThoriqAhnaf <muhamad.t.a@mail.ugm.ac.id>
2023-06-07 04:25:40 +0700

    fungsi bisa, saya yang gabisa melanjutkannya, sudah cukup, saya mau tidur tapi kalo tidur  gabisa bangun ntar
    pelan pelan pak sopir

---
+++ halodog-fe/components/navbar.jsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+function NavBar(props) {
+  const [navbar, setNavbar] = useState(false);
+  return (
+    <div>
+      <nav className="w-full bg-[#9914ad]">
+        <div className="justify-between px-4 mx-auto lg:px-40 md:items-center md:flex md:px-10">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              {/* LOGO */}
+              <Link href="/">
+                <h2 className="text-2xl text-cyan-600 font-bold ">
+                  <Image   src="/logo.png" width={200} height={200} alt="logo" />
+                </h2>
+              </Link>
+              {/* HAMBURGER BUTTON FOR MOBILE */}
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {/* {navbar ? (
+                    <Image src="/close.svg" width={30} height={30} alt="logo" />
+                  ) : (
+                    <Image
+                      src="/hamburger-menu.svg"
+                      width={30}
+                      height={30}
+                      alt="logo"
+                      className="focus:border-none active:border-none"
+                    />
+                  )} */}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? "p-12 md:p-0 block" : "hidden"
+              }`}
+            >
+              <ul className="h-screen md:h-auto gap-2 items-center justify-center md:flex ">
+                {/* <li
+                  className={`${
+                    props.menu === "dashboard"
+                      ? "bg-white"
+                      : "hover:bg-green-200"
+                  } text-black font-bold py-2 px-4 rounded-full`}
+                >
+                  <Link href="/" onClick={() => setNavbar(!navbar)}>
+                    Dashboard
+                  </Link>
+                </li> */}
+                <li
+                  className={`${
+                    props.menu === "upload" ? "bg-white" : "hover:bg-green-200"
+                  } text-white font-bold py-2 px-4 rounded-full`}
+                >
+                  <Link href="../diseases" onClick={() => setNavbar(!navbar)}>
+                    Diseases
+                  </Link>
+                </li>
+                <li
+                  className={`${
+                    props.menu === "history" ? "bg-white" : "hover:bg-green-200"
+                  } text-white font-bold py-2 px-4 rounded-full`}
+                >
+                  <Link href="animalspredict" onClick={() => setNavbar(!navbar)}>
+                Predict
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
--- halodog-fe/pages/diseases.jsx
+++ halodog-fe/pages/diseases.jsx
@@ -1,74 +1,105 @@
-import { axiosInstance } from "../utils/config";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../components/navbar.jsx";
+
+
 const DiseasesPage = ({ initialData }) => {
   const [data, setData] = useState(initialData);
   const [gejala, setGejala] = useState("");
-  const [data_response, setResponse] = useState([]);
+  const [data_response, setResponse] = useState("");
+  const [hewan, setHewan] = useState("");
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axiosInstance.get("/diseases");
+        const res = await axios.get("/diseases"); // Menggunakan axios tanpa instance
         setData(res.data);
       } catch (err) {
         console.error(err);
       }
     };
-    console.log(data);
+
     fetchData();
   }, []);
-  const handleFileInputChange = (e) => {
-    setGejala(e.target.value);
+
+  const handleHewanChange = (e) => {
+    setHewan(e.target.value);
+    console.log(hewan);
+  };
+  const handleGejalaChange = (e) => {
+    const inputValue = e.target.value;
+    const symptomsArray = inputValue
+    .split(",")
+    .map((symptom) => symptom.trim());
+  setGejala(symptomsArray);
+    setResponse(symptomsArray);
     console.log(gejala);
   };
 
   const handleSubmit = (e) => {
-    console.log(gejala);
     e.preventDefault();
 
     axios
-      .get(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/diseases/?symptoms=${gejala}`,
-        {}
-      )
+      .post(`${process.env.NEXT_PUBLIC_BASE_URL}/animals/predict-disease`, {
+        animal: hewan,
+        symptoms: gejala,
+      })
       .then((res) => {
-        // Authenticate MEMBUAT SET COOKIE TOKEN (JWT SECRET)
-        console.log(res.data);
+        console.log(res.data.data);
+
         setResponse(res.data);
+        console.log(data_response);
       })
       .catch((err) => {
         console.log(err.response);
         toast.error(err.response.data.errors);
       });
   };
+
   return (
     <div>
-      <h1>Diseases</h1>
-      <form
-        className="w-full h-[80%] justify-between items-center flex flex-col"
-        onSubmit={handleSubmit}
-      >
-        {data_response.success &&
-          data_response.data.map((disease, index) => (
-            <div key={index}>
-              <h2>{disease.name}</h2>
-              <p>{disease.description}</p>
-              <p>Gejala Umum : {disease.symptoms.join(", ")}</p>
+    <Navbar />
+    <div className="container px-4 md:px-10 lg:px-40 py-8 flex flex-col h-full justify-center">
+      <h1 className="text-2xl font-bold mb-4 text-black text-center">Diseases</h1>
+      <form onSubmit={handleSubmit}>
+        {data_response !== "" ? (
+          <>
+            {" "}
+            <div className="mb-4">
+
+              <p>Kemungkinan Penyakit : </p>
+              <h2 className="text-2xl font-bold">{data_response.data}</h2>
+
             </div>
-          ))}
+          </>
+        ) : (
+          <></>
+        )}
+
         <input
           type="text"
           id="text1"
-          onChange={handleFileInputChange}
-          className="border border-slate-200 shadow-md p-3 text-black"
+          onChange={handleHewanChange}
+          placeholder="Masukkan jenis hewan"
+          className="border mx-10 border-slate-200 shadow-md p-3 text-black mb-4"
+        />
+        <input
+          type="text"
+          id="text2"
+          onChange={handleGejalaChange}
+          placeholder="Masukkan gejala"
+          className="border border-slate-200 mx-10 shadow-md p-3 text-black mb-4"
         />
         <button
           type="submit"
-          className={`bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-800 transform transition duration-300`}
+          className="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-800 transition duration-300"
         >
           Upload
         </button>
       </form>
+    </div>
     </div>
   );
 };
@@ -76,7 +107,7 @@ const DiseasesPage = ({ initialData }) => {
 export async function getServerSideProps() {
   let data = {};
   try {
-    const res = await axiosInstance.get("/diseases"); // use the instance
+    const res = await axios.get("/diseases"); // Menggunakan axios tanpa instance
     data = res.data;
   } catch (err) {
     console.error(err);
